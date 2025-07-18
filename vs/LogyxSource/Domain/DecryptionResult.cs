04fa8b65015e84608a3a2a6f5329d1144a391733
hebelmx <hebelmx@gmail.com>
2025-07-18 03:10:08 -0600

    Uppdated git ignore

--- vs/LogyxSource/Domain/DecryptionResult.cs
+++
@@ -1,7 +0,0 @@
-namespace LogyxSource.Domain;
-
-public record DecryptionResult
-{
-    public string XmlContent { get; init; } = string.Empty;
-    public IReadOnlyList<string> Warnings { get; init; } = new List<string>();
-}
